2d1230021681d745f67e300a4dbe3f45443ab460
tipech <tipech@intracom-telecom.com>
2016-12-06 13:16:00 +0200

    Initial commit, splitting core and cloud configurations.

---
+++ src/main/java/eu/h2020/symbiote/CloudConfigServiceApplication.java
@@ -0,0 +1,14 @@
+package eu.h2020.symbiote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class CloudConfigServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudConfigServiceApplication.class, args);
+	}
+}
